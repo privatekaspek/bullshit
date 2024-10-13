e6150abdab74f8f5ef8ed79ed5b7e3f5fd930557
kaspek <84907738+privatekaspek@users.noreply.github.com>
2024-10-13 18:51:31 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,30 +1,21 @@
 function AsketLoad(product)
   createTimer(1000,function() AsketLoad = nil end)
   if not product then print('❓ Enter the product') return end
-  
+
   product = string.lower(product:gsub("^%s*(.-)%s*$", "%1"))
   print('')
   AsketVarPrivate = 'Payday20052512'
 
-  local users = loadstring(getInternet().getURL('https://raw.githubusercontent.com/sasyn-asket/bullshit/refs/heads/main/db.txt'))()
   local uuid = io.popen('wmic csproduct get uuid'):read('*a'):gsub('UUID',''):match"^%s*(.*)":match"(.-)%s*$"
+  local str = getInternet().getURL('http://185.128.106.216:8000/subscriptions?game_id=1&hvid='..uuid)
   local uname = os.getenv('USERNAME')
-  
+
   local f,fp,a = false,false
 
   local w,h = executeCodeLocalEx('user32.GetSystemMetrics',0),executeCodeLocalEx('user32.GetSystemMetrics',1)
-  
-  for i = 1, #users do
-    if users[i][1] == uuid then
-      f = true
-      a = i
-      for j = 2, #users[i] do
-        if product == users[i][j] then
-          fp = true
-        end
-      end
-    end
-  end
+
+  if string.len(str) >= 5 then f = true end
+  if product == 'lockdown protocol' then fp = true end
 
   if f then
     print('❓ HWID: '..uuid)
@@ -36,9 +27,7 @@ function AsketLoad(product)
 
   if f and product == '' then
     print('❓ Your products:')
-    for i = 2, #users[a] do
-      print(' - '..users[a][i])
-    end
+    print(' - lockdown protocol')
     return
   end
 
@@ -56,7 +45,7 @@ function AsketLoad(product)
     fi:close()
     loadTable(path)
     os.remove(path)
-    
+
     local ico = getInternet().getURL('https://github.com/privatekaspek/bullshit/raw/refs/heads/main/ico')
     path = data..'\\AsketIco'
     fi = io.open(path, 'w')
@@ -66,7 +55,7 @@ function AsketLoad(product)
     p.loadFromStream(createStringStream(ico))
     getApplication().Icon = p.getBitmap()
     os.remove(path)
-    
+
   elseif not f then
     print('✖ User not found \n'..uuid)
   elseif f and not fp then
